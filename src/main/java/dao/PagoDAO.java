c79d0fdbfb285a7f24bb9ee9958cb1e58ff59371
paulapanetta <paulapanetta7@gmail.com>
2026-07-27 19:10:55 -0300

    agrego DAO de pago

--- src/main/java/dao/PagoDAO.java
+++ src/main/java/dao/PagoDAO.java
@@ -1,4 +1,16 @@
 package dao;
 
+import model.pago.Pago;
+
+import java.util.List;
+
 public interface PagoDAO {
+
+    void guardar(Pago pago);
+    Pago buscarPorId(int id);
+    List<Pago> obtenerTodos();
+    List<Pago> obtenerPorOrden(int ordenId);
+    void actualizar(Pago pago);
+    void eliminar(int id);
+
 }
